34598877554a8d8651163340649b42bc83726a1d
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-03-01 18:04:16 -0700

    merged loginaction & signupaction files into a singular authActions.js file

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,9 @@
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
+export const SIGNUP_START = 'SIGNUP_START';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
 export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
@@ -22,4 +26,22 @@ export const login = credentials => dispatch => {
     })
   );
 };
-// login is going to break the code until I can get those endpoints. add friends server to test tomorrow if I get time.
+
+export const signup = newUser => dispatch => {
+  console.log('signup action firing');
+  dispatch({ type: SIGNUP_START });
+  return (
+    axiosWithAuth()
+    .post('/auth/register', newUser)
+    .then(res => {
+      console.log('signup action.then', res);
+      // localStorage.setItem('token', res.data.token);
+      // not sure I need the above in signup, only login^^
+      dispatch({ type: SIGNUP_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log('Signup action error', err);
+      dispatch({ type: SIGNUP_FAILURE, payload: err });
+    })
+  );
+};
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,11 @@ export {
   LOGIN_START, 
   LOGIN_SUCCESS, 
   LOGIN_FAILURE,
-} from './loginAction';
-
-export { 
   signup,
   SIGNUP_START,
   SIGNUP_SUCCESS,
   SIGNUP_FAILURE,
-} from './signupAction';
+} from './authActions';
 
 export {
   FETCH_VALUE_START,
