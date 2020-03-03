4d23884574609089ba6296753890db535a170558
Javier Umana <javierumanafs@gmail.com>
2020-03-03 17:11:48 -0500

    pulled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import EditUser from './components/EditUser';
-=======
 import { Route, Switch, Link } from 'react-router-dom';
->>>>>>> 973f83bed7632876050a603e7bcb0abdc129d7a2
 
 import PrivateRoute from './components/PrivateRoute';
 
@@ -19,17 +14,12 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-<<<<<<< HEAD
-      <EditUser />
-      <Route exact path='/' component={Login} />
-      <Route path='/signup' component={Signup} />
-=======
       <h1>Essentialism</h1>
       <Switch>
         <Route exact path='/' component={Login} />
 
         <Route path='/signup' component={Signup} />
-        
+
         <PrivateRoute exact path='/select-values' component={SelectValues} />
         {/* <PrivateRoute path='/values-dashboard' />
         <PrivateRoute path='/dashboard' /> */}
@@ -37,7 +27,6 @@ function App() {
 
 
       </Switch>
->>>>>>> 973f83bed7632876050a603e7bcb0abdc129d7a2
 
     </div>
   );
