0673027bad5b233168d792e70f3fcec9d47af6b0
Austin Huisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-03-03 14:26:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import EditUser from './components/EditUser';
-=======
 import { Route, Switch, Link } from 'react-router-dom';
->>>>>>> 973f83bed7632876050a603e7bcb0abdc129d7a2
 
 import PrivateRoute from './components/PrivateRoute';
 
@@ -19,11 +16,9 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-<<<<<<< HEAD
       <EditUser />
       <Route exact path='/' component={Login} />
       <Route path='/signup' component={Signup} />
-=======
       <h1>Essentialism</h1>
       <Switch>
         <Route exact path='/' component={Login} />
@@ -35,10 +30,7 @@ function App() {
         <PrivateRoute path='/dashboard' /> */}
         {/* <PrivateRoute path='/' /> */}
 
-
       </Switch>
->>>>>>> 973f83bed7632876050a603e7bcb0abdc129d7a2
-
     </div>
   );
 }
